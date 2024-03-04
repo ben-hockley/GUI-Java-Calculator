b413e9011ef4f7f6fd207eabb5ce8f77e5a1d3aa
c23037255 <hockleyb@cardiff.ac.uk>
2024-03-04 20:17:17 +0000

    Created new class Calculator extends JFrame.

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Calculator extends JFrame {
+    Calculator(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+}
