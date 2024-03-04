1f69fb1dd764982ca1217c2a370b27f7640eed5a
c23037255 <HockleyB@cardiff.ac.uk>
2024-03-04 23:40:47 +0000

    Added ImageIcon to calculator.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
 
         Calculator myCalculator = new Calculator();
         myCalculator.setTitle("Java Calculator");
+
+        Image calculatorImage = new ImageIcon("calculator.jpg").getImage();
+        myCalculator.setIconImage(calculatorImage);
     }
 }
